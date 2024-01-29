dca154d78ab3ff9144b66d4d2f56df1d14ffe84d
chandra7730s <153893650+chandra7730s@users.noreply.github.com>
2024-01-29 08:16:48 +0530

    Add files via upload

---
+++ # python assignment 3.py
@@ -0,0 +1,7 @@
+# python assignment 3
+# use else block to display a message "Done"after successful execuation of foor loop
+numbers=[1,3,4,5,6]
+for number in numbers:
+    print(numbers)
+else:
+    print("loop completed successful.done!")
