ce431667f24862d4115870ea61d6f44b9b2e710c
chandra7730s <153893650+chandra7730s@users.noreply.github.com>
2024-02-02 14:43:00 +0530

    Add files via upload

---
+++ # paratics and assignment.py
@@ -0,0 +1,22 @@
+# paratics and assignment
+s="this is aman"
+print(s.isalnum())
+
+s="this is Shandra"
+print(s.isalpha())
+
+s="THIS is chandra"
+print(s.isdigit())
+
+s="this is chandra"
+print(s.islower())
+
+s="this is @ ramu"
+print(s.isupper())
+
+s="this is man"
+print(s.istittlecase())
+
+s="this is ramu"
+print(s.isspace())
+
