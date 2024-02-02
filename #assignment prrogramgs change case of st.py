1a246e85019839ed11b290498189f99c969b81e7
chandra7730s <153893650+chandra7730s@users.noreply.github.com>
2024-02-02 14:22:55 +0530

    Add files via upload

---
+++ #assignment prrogramgs change case of st.py
@@ -0,0 +1,35 @@
+#assignment prrogramgs change case of string
+s="this is aman"
+print(s.upper()) #THIS IS AMAN
+print(s.lower()) #this is aman
+x="python is easy"
+y=x.upper()
+print(x)
+print(y)
+
+x="pythin is @123 esay"
+y=x.lower
+print(x)
+print(y)
+
+x='qcdfr'
+y=x.swapcase()
+print(x) #qcdfr
+print(y) #QCDFR
+
+x="python is Easy"
+y=x.capitalize()
+print(x) #python is Easy
+print(y) #python is easy
+
+x="pythin is EAsy"
+y=x.title()
+print(x)
+print(y)
+
+
+
+#type case of present string
+s="this is aman"
+print(s.isalnum())
+print("aman 900".isspace())
